2616fd97940e451cb4e349427b60ad106f789ec4
dhakalmahima188 <dhakalmahima18@gmail.com>
2023-06-06 11:45:28 +0545

    added status

--- loginapp/views.py
+++ loginapp/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.contrib.auth.models import User
+from rest_framework import status
 from django.contrib.auth import authenticate, login
 from .serializers import UserSerializer
 
